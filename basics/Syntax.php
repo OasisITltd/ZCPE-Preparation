82edb46929361f3a2e4ce3694d8dcaab270d9190
mobarakali <mobarak.ali@gmail.com>
2016-03-28 13:32:38 +0600

    Changes made from my office pc

--- basics/Syntax.php
+++ basics/Syntax.php
@@ -5,6 +5,12 @@
     <title>Document</title>
 </head>
 <body>
-
+<h1>Basic syntax</h1>
+<ul>
+    <li>PHP tags</li>
+    <li>Escaping from HTML</li>
+    <li>Instruction separation</li>
+    <li>Comments</li>
+</ul>
 </body>
 </html>
