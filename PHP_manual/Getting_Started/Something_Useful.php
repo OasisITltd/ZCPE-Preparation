e58ae9ddf397b1fb19a366897fc08b721380e1a2
Mobarak <mobarak.ali@gmail.com>
2016-03-29 01:13:47 +0600

    Learning form home!

--- PHP_manual/Getting_Started/Something_Useful.php
+++ PHP_manual/Getting_Started/Something_Useful.php
@@ -3,7 +3,10 @@
 <?php echo $_SERVER['HTTP_USER_AGENT'];?>
 <h3>Example #2 Example using control structures and functions</h3>
 <?php
-    if(strpos($_SERVER['HTTP_USER_AGENT'],'msie')!==false){
+    if(strpos($_SERVER['HTTP_USER_AGENT'],'MSIE')!==false||
+        strpos($_SERVER['HTTP_USER_AGENT'],'Trident')!== false){
         echo"You are using Internet Explorer.";
-    }
+    } else echo"NO! It's not IE!";
 
+?>
+<p>To be continued...</p>
