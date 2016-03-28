271dda0113e4eebcf2876349e6bdc53f3b59ec21
Mobarak <mobarak.ali@gmail.com>
2016-03-29 00:37:52 +0600

    Learning form home!

---
+++ PHP_manual/Getting_Started/Something_Useful.php
@@ -0,0 +1,9 @@
+<h1>Something Useful</h1>
+<h3>Example #1 Printing a variable (Array element)</h3>
+<?php echo $_SERVER['HTTP_USER_AGENT'];?>
+<h3>Example #2 Example using control structures and functions</h3>
+<?php
+    if(strpos($_SERVER['HTTP_USER_AGENT'],'msie')!==false){
+        echo"You are using Internet Explorer."
+    }
+
