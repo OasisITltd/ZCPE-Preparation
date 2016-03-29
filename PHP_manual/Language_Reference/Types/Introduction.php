0f6d4c0527e833f586a14fe89e4d5f6b53d5f79d
Mobarak <mobarak.ali@gmail.com>
2016-03-30 00:47:57 +0600

    Some info about my work progress!

---
+++ PHP_manual/Language_Reference/Types/Introduction.php
@@ -0,0 +1,31 @@
+<!-- PHP Manual > Language Reference > Types -->
+<pre>
+PHP supports eight primitive types.
+
+Four scalar types:
+
+boolean
+integer
+float (floating-point number, aka double)
+string
+Two compound types:
+
+array
+object
+And finally two special types:
+
+resource
+NULL
+This manual also introduces some pseudo-types for readability reasons:
+
+mixed
+number
+callback (aka callable)
+array|object
+void
+And the pseudo-variable $....
+
+Some references to the type "double" may remain in the manual. Consider double the same as float; the two names exist only for historic reasons.
+
+The type of a variable is not usually set by the programmer; rather, it is decided at runtime by PHP depending on the context in which that variable is used.
+</pre>
