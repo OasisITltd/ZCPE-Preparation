1761619a5b3ed160549811c78bfc8a45d01159c4
Mobarak <mobarak.ali@gmail.com>
2016-04-03 09:12:05 +0600

    2 April 2016

--- PHP_manual/Language_Reference/Types/Integers.php
+++ PHP_manual/Language_Reference/Types/Integers.php
@@ -1,3 +1,17 @@
+<pre>
+    Integers can be specified in
+    decimal (base 10),
+    hexadecimal (base 16),
+    octal (base 8) or
+    binary (base 2) notation,
+
+    optionally preceded by a sign (- or +).
+    Binary integer literals are available since PHP 5.4.0.
+</pre>
+
+
+
+
 <?php
 
 ?>
