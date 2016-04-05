a4aa65192b6972a0ed074aa43539428344b53a7c
Mobarak <mobarak.ali@gmail.com>
2016-04-05 23:23:06 +0600

    Type Casting on April 5

---
+++ PHP_manual/Language_Reference/Types/type-juggling.php
@@ -0,0 +1,16 @@
+<h1>type-juggling</h1>
+<pre>
+<?php
+$foo = '0';
+var_dump($foo);
+$foo +=2;
+var_dump($foo);
+$foo +=1.3;
+var_dump($foo);
+$foo = $foo + "3 Eggs.";
+var_dump($foo);
+$foo = "4 Eggs"+9;
+var_dump($foo);
+?>
+    </pre>
+<!-- To Be Continued -->
