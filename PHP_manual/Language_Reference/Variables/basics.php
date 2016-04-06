6b089874c8fa20e9de8abe765ccf0ae6f4af92b8
Mobarak <mobarak.ali@gmail.com>
2016-04-06 21:36:03 +0600

    Type Casting  &  Type Juggling on April 5

--- PHP_manual/Language_Reference/Variables/basics.php
+++ PHP_manual/Language_Reference/Variables/basics.php
@@ -1,6 +1,15 @@
 <h1>PHP Variable Basics</h1>
-<pre>
+<p>
 Variables in PHP are represented by a dollar sign followed by the name of the variable. The variable name is case-sensitive.
+    </p>
+<p>
 
 Variable names follow the same rules as other labels in PHP. A valid variable name starts with a letter or underscore, followed by any number of letters, numbers, or underscores. As a regular expression, it would be expressed thus: '[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*'
-</pre>
+</p>
+
+<?php
+$var = "a variable";
+$var_?? = "will not work!";
+ echo $var, $var_??;
+?>
+
