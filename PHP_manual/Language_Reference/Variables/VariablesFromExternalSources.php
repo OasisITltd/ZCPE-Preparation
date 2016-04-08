745ea05f29cd9b9137f6ada99271a7b5a3e4d39e
Mobarak <mobarak.ali@gmail.com>
2016-04-09 00:02:30 +0600

    Variables From External Sources Will be covered later while working with form data.

---
+++ PHP_manual/Language_Reference/Variables/VariablesFromExternalSources.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mobarak
+ * Date: 09-Apr-16
+ * Time: 12:00 AM
+ */
+
+/*
+ * Date: 9/4/2016
+ * This Section will covered at later time
+ * When working with forms.*/
