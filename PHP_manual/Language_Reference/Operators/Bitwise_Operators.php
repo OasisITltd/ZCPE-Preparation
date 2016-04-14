9ea92ff99303495b0aedcf44772c997434e265f6
Mobarak <mobarak.ali@gmail.com>
2016-04-14 22:43:57 +0600

    Bitwise operator!

---
+++ PHP_manual/Language_Reference/Operators/Bitwise_Operators.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mobarak
+ * Date: 14-Apr-16
+ * Time: 10:41 PM
+ */
+
+/* This is one place where I have to improve more.
+In fact I'm very week at this concept
+So, It's High Time I should try my best
+Just not to fill the day..............*/
