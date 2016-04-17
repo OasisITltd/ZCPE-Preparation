8fd4a6b654cac2396cb1cbcbcdf855d155129284
Mobarak <mobarak.ali@gmail.com>
2016-04-18 02:05:41 +0600

    Learning about "String".

--- PHP_manual/Language_Reference/Types/string/Strings.php
+++ PHP_manual/Language_Reference/Types/string/Strings.php
@@ -9,7 +9,21 @@
 function html($a,$b ){
     echo "<$b> $a </$b>";
 }
-
+function br(){echo "<br>";}
 $b = 'Learning about "String".';
 
 html($b,"h1"); // <h1> Learning about "String".</h1>
+
+echo 'This is a simple string.';
+br();
+echo " A string can
+have multiple
+line like this with newlines,
+but will display in one line
+on the browser.";
+
+br();
+echo ' Showing quote is \' tricky\' here!';
+br();
+echo 'To write a multiy line comment in
+ php you have to use \\* commentd text *\\';
